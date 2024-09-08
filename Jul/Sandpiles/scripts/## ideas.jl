0ec0450d0e38a2fca2803c6cdb8d8d0f5d4b6cf4
Kanga <j.reubender@gmail.com>
2024-09-08 13:17:46 +1000

    incl sims

--- Jul/Sandpiles/scripts/## ideas.jl
+++ Jul/Sandpiles/scripts/## ideas.jl
@@ -3,3 +3,6 @@
 #Tumor growth
 ## sand piling on continuous surface (no edges) (e.g. a sphere - cell shape? )
 ## reduce height all cells by ~1 each time step to avoid overall growth
+
+
+##dropping all grains of sand at once
--- Jul/Sandpiles/scripts/same_n_reps.jl
+++ Jul/Sandpiles/scripts/same_n_reps.jl
@@ -1,11 +1,24 @@
 ## Many simulations of the same model, but with random sand drops
 include("../src/Sandpiles.jl")
 using CSV
-n_sims = 30
+n_sims = 100
 
-for i in 1:n_sims
-    log = Sandpiles.simulate_sandpile((20,20); drop_placement = "rand")
-    CSV.write("./data/simulation_20x20_rep_$i.csv", log)
+
+function simulate_n_sandpiles(n_sims, size::Tuple)
+    x = size[1]
+    y = size[2]
+    for i in 1:n_sims
+        log = Sandpiles.simulate_sandpile((x,y); drop_placement = "rand")
+        CSV.write("./data/simulation_$(x)x$(y)_rep_$i.csv", log)
+    end
+end
+function simulate_n_sandpiles(n_sims, size::Int)
+    x = size
+    y = size
+    for i in 1:n_sims
+        log = Sandpiles.simulate_sandpile((x,y); drop_placement = "rand")
+        CSV.write("./data/simulation_$(x)x$(y)_rep_$i.csv", log)
+    end
 end
 
-
+simulate_n_sandpiles(100, 50)
